21d21d8367621852b70b8c3cfb66e19e9c3027ed
YovoManolov <yovo.manolov@abv.bg>
2019-08-05 00:12:01 +0300

    update 05.08.19
    update 05.08.19

---
+++ vtgw/src/main/java/com/varnaTravelGuideWeb/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.varnaTravelGuideWeb.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+         .anyRequest().authenticated()
+         .and()
+         .oauth2Login();
+    }
+}
--- vtgw/src/main/java/com/varnaTravelGuideWeb/repository/UserRepository.java
+++ vtgw/src/main/java/com/varnaTravelGuideWeb/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.varnaTravelGuideWeb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.varnaTravelGuideWeb.model.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User,String>{
+	
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
 }
