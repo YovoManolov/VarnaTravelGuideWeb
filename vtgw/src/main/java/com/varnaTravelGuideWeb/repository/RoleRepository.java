7da57b545ebd4e61f2974fda88c7957297b4cf9b
YovoManolov <yovo.manolov@abv.bg>
2019-11-28 21:03:24 +0200

    clean config 28.11.19

--- vtgw/src/main/java/com/varnaTravelGuideWeb/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.varnaTravelGuideWeb.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.varnaTravelGuideWeb.domain.Role;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-    Role findByRole(String role);
-}
