d467e5ebb82b1dcbc3a2e10904681fd95c2f3839
YovoManolov <yovo.manolov@abv.bg>
2019-11-28 21:03:37 +0200

    clean config

---
+++ vtgw/src/test/java/com/varnaTravelGuideWeb/vtgw/VtgwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varnaTravelGuideWeb.vtgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtgwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
