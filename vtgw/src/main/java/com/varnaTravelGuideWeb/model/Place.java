40468dc7de023e73b1ba7e1e6474126c5740807d
YovoManolov <yovo.manolov@abv.bg>
2020-02-12 22:04:10 +0200

    commit all changes 22.04.06

--- vtgw/src/main/java/com/varnaTravelGuideWeb/model/Place.java
+++ vtgw/src/main/java/com/varnaTravelGuideWeb/model/Place.java
@@ -11,21 +11,31 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Document
+@Document(collection="places")
 public class Place {
 	
 	@Id
 	@Field("_id")
 	private String id;
+	@Field("name")
     private String name;	
+	@Field("address")
     private String address;
+	@Field("latitude")
     private Double latitude;
+	@Field("longitude")
     private Double longitude;
+	@Field("contacts")
     private String contacts;
+	@Field("description")
     private String description;
+	@Field("typeOfPlace")
     private Integer typeOfPlace;
+	@Field("priceCategory")
     private Integer priceCategoryId;
+	@Field("images")
     private ArrayList<Image> images;
+	@Field("workHours")
     private WorkHours workHours;
 
 	
