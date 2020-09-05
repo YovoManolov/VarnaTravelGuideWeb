491523a7bd48ea9714d08a4da3081b26e823322b
Yovo Manolov <trance96@abv.bg>
2020-09-05 22:56:42 +0300

    Commit static util objects in class StaticTestObjects.java and update to class PlaceServiceImplT new method
    05.09.2020

--- vtgw/src/test/java/com/varnaTravelGuideWeb/vtgw/HotelServicesImplT.java
+++ vtgw/src/test/java/com/varnaTravelGuideWeb/vtgw/HotelServicesImplT.java
@@ -1,12 +1,11 @@
 package com.varnaTravelGuideWeb.vtgw;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,17 +13,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.varnaTravelGuideWeb.exception.RecordNotFoundException;
 import com.varnaTravelGuideWeb.model.Hotel;
-import com.varnaTravelGuideWeb.model.Place;
-import com.varnaTravelGuideWeb.model.utilModels.Image;
-import com.varnaTravelGuideWeb.model.utilModels.WorkHours;
 import com.varnaTravelGuideWeb.repository.HotelRepository;
 import com.varnaTravelGuideWeb.service.impl.HotelServiceImpl;
 import com.varnaTravelGuideWeb.service.impl.PlaceServiceImpl;
+import com.varnaTravelGuideWeb.vtgw.utilClasses.StaticTestObjects;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -41,38 +37,17 @@ public class HotelServicesImplT {
 	@Autowired 
 	HotelServiceImpl hotelServiceImpl;
 
-	private static final String PLACE_ID_HEX = "5c72c5d22c0f1304b47a2818";
-	
-	private Optional<Hotel> optionalHotel = Optional.of(
-			new Hotel("hotel1Id", new ObjectId(PLACE_ID_HEX), 3,null));
-	
-	private static final Place placeToReturn = new Place(
-											"5c72c5d22c0f1304b47a2818",
-											"Hotel_name",
-											"hotel_address",
-											new GeoJsonPoint(11.11, 22.22),
-											"contacts",
-											"description",
-											2, // hotel
-											Arrays.asList(
-													new Image("imageUrl", 1),
-													new Image("imageUrl2", 2)
-											),
-											new WorkHours(true, "", "", ""),
-											"BUDGET"
-										);
-
 	@Test
 	public void getHotelByIdTest() {
 		try {
-			when(hotelRepository.findById("hotel1Id")).thenReturn(optionalHotel);
+			when(hotelRepository.findById("hotel1Id")).thenReturn(StaticTestObjects.optionalHotel);
 			
-			when(placeServiceImpl.getPlaceById(PLACE_ID_HEX))
-			.thenReturn(placeToReturn);
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_HEX))
+			.thenReturn(StaticTestObjects.placeToReturn);
 			
 			assertEquals(
 					"com.varnaTravelGuideWeb.model.Hotel", 
-					hotelServiceImpl.getHotelById(PLACE_ID_HEX).getClass().getName()
+					hotelServiceImpl.getHotelById(StaticTestObjects.PLACE_ID_HEX).getClass().getName()
 			);
 			
 			
@@ -85,13 +60,46 @@ public class HotelServicesImplT {
 	public void getHotelByIdExceptionT() {
 
 		try {
-			when(hotelRepository.findById("hotel1Id")).thenReturn(optionalHotel);
-			when(placeServiceImpl.getPlaceById(PLACE_ID_HEX)).thenReturn(placeToReturn);
+			when(hotelRepository.findById("hotel1Id"))
+						.thenReturn(StaticTestObjects.optionalHotel);
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_HEX))
+						.thenReturn(StaticTestObjects.placeToReturn);
 			 
 			hotelServiceImpl.getHotelById("hotelTestException");
 			exceptionRule.expect(RecordNotFoundException.class);
 		} catch (RecordNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void getAllHotelsT() {
+		try {
+			
+			when(hotelRepository.findAll()).
+					thenReturn(StaticTestObjects.mockedHotelList);
+			
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_1_HEX)).
+					thenReturn(StaticTestObjects.placeToReturn);
+			
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_2_HEX)).
+					thenReturn(StaticTestObjects.placeToReturn);
+			
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_3_HEX)).
+					thenReturn(StaticTestObjects.placeToReturn);
+			
+			when(placeServiceImpl.getPlaceById(StaticTestObjects.PLACE_ID_4_HEX)).
+					thenReturn(StaticTestObjects.placeToReturn);
+			
+		   List<Hotel> allHotels = hotelServiceImpl.getAllHotels();
+		   for(Hotel h : allHotels) {
+			  assertNotNull(h.getPlace());
+			  assertEquals(StaticTestObjects.PLACE_ID_HEX, h.getPlace().get_id());
+		   }
+		   
+		} catch (RecordNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
